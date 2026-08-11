0ace31f9071969825b75187d31c5f418212e9ae9
domannand95-svg <domannand95@gmail.com>
2026-08-11 21:16:06 +1000

    Translate Sovereign metadata fail closed (#14)

---
+++ tooling/integration/__init__.py
@@ -0,0 +1,5 @@
+"""Explicit, versioned integration boundaries owned by BKI."""
+
+from .sovereign_profile import ProfileTranslationError, translate_frontmatter
+
+__all__ = ["ProfileTranslationError", "translate_frontmatter"]
